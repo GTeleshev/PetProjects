1b86da4724a0c2f4f39635b1919a9b4f7becc3a8
Grigory Teleshev <Grigory.Teleshev@yandex.ru>
2022-11-29 22:58:16 +0300

    Matrices multiply, quaternion class (add, multiply) added

---
+++ C#/Functions/Matrices/Multiply/Program.cs
@@ -0,0 +1,88 @@
+﻿/* Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18 */
+int sogl = 4;
+double[,] firstMatrix = GetArray(4, sogl, 1, 5);
+double[,] secondMatrix = GetArray(sogl, 1, 1, 5);
+double[,] multi = MatrixMultiply(firstMatrix, secondMatrix);
+Console.WriteLine();
+PrintArray(firstMatrix);
+Console.WriteLine();
+PrintArray(secondMatrix);
+Console.WriteLine();
+PrintArray(multi);
+
+Console.WriteLine(multi[0,0]);
+Console.WriteLine(multi[1,0]);
+Console.WriteLine(multi[2,0]);
+Console.WriteLine(multi[3,0]);
+
+double[,] MatrixMultiply(double[,] Matrix1, double[,] Matrix2)
+{
+    int rows1 = Matrix1.GetLength(0);
+    int columns1 = Matrix1.GetLength(1);
+    int rows2 = Matrix2.GetLength(0);
+    int columns2 = Matrix2.GetLength(1);
+    double[,] multipliedMatrix = new double[rows1, columns2];
+    double sumij = 0;
+    for (int i = 0; i < rows1; i++)
+    {
+        for (int j = 0; j < columns2; j++)
+        {
+            sumij = 0;
+            for (int r = 0; r < columns1; r++)
+            {
+                sumij = sumij + Matrix1[i, r] * Matrix2[r, j];
+            }
+            multipliedMatrix[i, j] = sumij;
+        }
+    }
+    return multipliedMatrix;
+}
+
+/* Операция умножения двух матриц выполнима только в том случае, 
+если число столбцов в первом сомножителе равно числу строк во втором; 
+в этом случае говорят, что матрицы согласованы. В частности, умножение 
+всегда выполнимо, если оба сомножителя — квадратные матрицы одного и 
+того же порядка.
+ */
+
+
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int interimVar = 0;
+    if (minValue > maxValue)
+    {
+        interimVar = maxValue;
+        maxValue = minValue;
+        minValue = interimVar;
+    }
+
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int rand = new Random().Next(minValue, maxValue + 1);
+            result[i, j] = Convert.ToDouble(rand);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ C#/Functions/Quaternions/Program.cs
@@ -0,0 +1,118 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Quaternion Class Check");
+Quaternion quater = new Quaternion(1, 2, 3, -4);
+// string name = Quaternion.PrintQ(quater);
+Console.WriteLine(quater.a);
+Console.WriteLine(quater.b);
+Console.WriteLine(quater.c);
+Console.WriteLine(quater.d);
+// Console.WriteLine(quater.PrintQ());
+Console.WriteLine(quater.PrintQ());
+Console.ReadLine();
+Quaternion quater2 = new Quaternion(1, 3, 5, 6);
+Console.WriteLine(quater2.PrintQ());
+Quaternion quater3 = quater.QuatAdd(quater2);
+Console.WriteLine(quater3.PrintQ());
+Quaternion quater4 = quater.MultiplyQuat(quater2);
+Console.WriteLine(quater4.PrintQ());
+public class Quaternion
+{
+    public double a { get; set; }
+    public double b { get; set; }
+    public double c { get; set; }
+    public double d { get; set; }
+
+    public Quaternion()
+    {
+        this.a = 0;
+        this.b = 0;
+        this.c = 0;
+        this.d = 0;
+    }
+
+    public Quaternion(double A, double B, double C, double D)
+    {
+        this.a = A;
+        this.b = B;
+        this.c = C;
+        this.d = D;
+    }
+
+    public string PrintQ()
+    {
+        string fs = "+";
+        string ss = "+";
+        string ts = "+";
+        if (this.b < 0)
+        {
+            fs = "-";
+        }
+        if (this.c < 0)
+        {
+            ss = "-";
+        }
+        if (this.d < 0)
+        {
+            ts = "-";
+        }
+        double absB = Math.Abs(this.b);
+        double absC = Math.Abs(this.c);
+        double absD = Math.Abs(this.d);
+        string quaternString = $"{this.a} {fs} {absB}i {ss} {absC}j {ts} {absD}k";
+        return quaternString;
+    }
+
+    public Quaternion QuatAdd(Quaternion quat2)
+    {
+        Quaternion sumQuat = new Quaternion(this.a + quat2.a, this.b + quat2.b, this.c + quat2.c, this.d + quat2.d);
+        return sumQuat;
+    }
+
+    public Quaternion MultiplyQuat(Quaternion quat2)
+    {
+        Quaternion productQuat = new Quaternion();
+        double[,] firstMatrix = {
+            {this.a, -this.b, -this.c, -this.d},
+            {this.b, this.a, -this.d, this.c},
+            {this.c, this.d, this.a, -this.b},
+            {this.d, -this.c, this.b, this.a}
+        };
+        double[,] secondMatrix = {
+            {quat2.a},
+            {quat2.b},
+            {quat2.c},
+            {quat2.d}
+        };
+        double[,] productMatrix = MatrixMultiply(firstMatrix, secondMatrix);
+        productQuat.a = productMatrix[0,0];
+        productQuat.b = productMatrix[1,0];
+        productQuat.c = productMatrix[2,0];
+        productQuat.d = productMatrix[3,0];
+        
+        return productQuat;
+    }
+    double[,] MatrixMultiply(double[,] Matrix1, double[,] Matrix2)
+    {
+        int rows1 = Matrix1.GetLength(0);
+        int columns1 = Matrix1.GetLength(1);
+        int rows2 = Matrix2.GetLength(0);
+        int columns2 = Matrix2.GetLength(1);
+        double[,] multipliedMatrix = new double[rows1, columns2];
+        double sumij = 0;
+        for (int i = 0; i < rows1; i++)
+        {
+            for (int j = 0; j < columns2; j++)
+            {
+                sumij = 0;
+                for (int r = 0; r < columns1; r++)
+                {
+                    sumij = sumij + Matrix1[i, r] * Matrix2[r, j];
+                }
+                multipliedMatrix[i, j] = sumij;
+            }
+        }
+        return multipliedMatrix;
+    }
+}
